ad194412e9f7123add3a43edfd0576f39b591843
Nathan Huse <nathan.huse@yahoo.com>
2021-06-15 14:44:22 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -22,12 +22,8 @@ function pressIt() {
 function submitIt() {
   $('form').on('submit', function() {
     if ($('input:first').val() === 'correct') {
-      alert('Your form is going to be submitted now.');
-      return;
-    }
-    alert('you entered the wrong value');
+    alert('Your form is going to be submitted now.');
     return;
-  })
 }
 
 $(document).ready(function(){
