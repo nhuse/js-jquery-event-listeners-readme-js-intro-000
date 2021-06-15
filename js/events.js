d17b5f09a45b4640656b51f566024ae63927e50a
Nathan Huse <nathan.huse@yahoo.com>
2021-06-15 14:37:47 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,23 @@
 //define functions here
+function getIt () {
+  $('p').on('click', function() {
+    alert('Hey!')
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if(key.which == 71) {
+      alert('G key pressed')
+    }
+  })
+}
 
 $(document).ready(function(){
 
